d290a0df0ec2d1afb512c8f6181017942d4ede55
davirlein1988 <lein.davir@gmail.com>
2020-05-11 22:35:33 -0500

    Form  example for guidance

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-<form name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
-  Name: <input type="text" name="fname">
-  <input type="submit" value="Submit">
-</form>  
+const init = function() {
+  document.getElementById('submit-btn').addEventListener('click', send);
+}
+const send = function(evt) {
+  evt.preventDefault()
+  evt.stopPropagation()
+  let ret = validate()
+  if(ret) {
+    document.getElementById('userData').submit()
+  }
+}
+
+const validate = function(evt) {
+  let valid = true
+  let name = document.getElementById('contact-name')
+  let email = document.getElementById('contact-email')
+  let msg = document.getElementById('contact-msg')
+  if(name.value !== "" || email.value !== "" || msg.value !== "") {
+    valid = false
+  }
+  
+  document.getElementById('name-heading').textContent = `Hi ${name.value}`
+  document.getElementById('player-email').textContent = email.value
+  document.getElementById('player-theme').textContent = msg.value
+  let formContainer = document.getElementById('form-wrapper')
+  formContainer.parentElement.classList.add('hidden')
+  return valid
+}
+
+document.addEventListener('DOMContentLoaded', init);
